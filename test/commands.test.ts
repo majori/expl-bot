7d72ce0f8dd9ad63ba81686d38f43e967bc32906
Antti Kivimäki <antti.kivimaki@futurice.com>
2018-08-05 15:39:19 +0300

    Create test for testing expl indexing

--- test/commands.test.ts
+++ test/commands.test.ts
@@ -1,8 +1,9 @@
 import 'mocha';
 import { expect } from 'chai';
+import * as _ from 'lodash';
 import config from '../src/config';
 import * as commands from '../src/commands';
-import { message } from './utils/context';
+import { message, USER_ID } from './utils/context';
 import { knex, clearDb } from './helper';
 
 describe('Commands', () => {
@@ -10,13 +11,13 @@ describe('Commands', () => {
   after(async () => knex.destroy());
 
   describe('/expl', () => {
-    it('will respond with error if expl not found', async () => {
+    it('respond with error if expl not found', async () => {
       const ctx = message('?? NOT_FOUND');
       await commands.getExpl(ctx);
       expect(ctx.reply.lastArg).to.equal('Expl not found.');
     });
 
-    it('will get an existing expl', async () => {
+    it('get an existing expl', async () => {
       const expl = {
         key: 'key',
         value: 'value',
@@ -31,5 +32,37 @@ describe('Commands', () => {
       await commands.getExpl(ctx);
       expect(ctx.reply.lastArg).to.equal(`${expl.key}: ${expl.value}`);
     });
+
+    it('correctly gets indexed expl', async () => {
+      const users = [USER_ID, 223456789, 323456789, 423456789];
+
+      // Make user to be in the same group
+      await knex('auth')
+        .insert(_.map(users, (user) => ({
+          user_id: user,
+          chat_id: -123456789,
+        })));
+
+      const KEY = 'key';
+      const expls = _.times(4, (num) => ({
+        key: KEY,
+        value: num.toString(),
+        user_id: users[num],
+      }));
+
+      await knex('expls').insert(expls);
+
+      for (const [index, user] of users.entries()) {
+        const ctx = message(`?? ${KEY} ${index + 1}`);
+        await commands.getExpl(ctx);
+
+        expect(ctx.reply.lastArg).to.equal(`${KEY}: ${index}`);
+      }
+    });
+  });
+
+  describe('/add', () => {
+    it('creates expl');
+    it('prevents user to create multiple expls with same key');
   });
 });
--- test/utils/context.ts
+++ test/utils/context.ts
@@ -15,15 +15,18 @@ const baseContext = (state?: any) => {
   };
 };
 
+export const USER_ID = 123456789;
+export const GROUP_ID = -1001184985530;
+
 export const message = (msg: string, fromGroup?: boolean): any => {
   const chat = fromGroup ?
     {
-      id: -1001184985530,
+      id: GROUP_ID,
       title: 'expl-bot dev group',
       type: 'supergroup',
     } :
     {
-      id: 123456789,
+      id: USER_ID,
       first_name: 'Test',
       last_name: 'User',
       username: 'testuser',
@@ -31,13 +34,13 @@ export const message = (msg: string, fromGroup?: boolean): any => {
 
   return {
     ...baseContext({
-      user: 123456789,
+      user: USER_ID,
       chat: chat.id,
     }),
     message: {
       message_id: 1234,
       from: {
-        id: 123456789,
+        id: USER_ID,
         is_bot: false,
         first_name: 'Test',
         last_name: 'User',
