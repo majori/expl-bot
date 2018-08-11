afe7fb70ea979424c4bad959e60834e0c22d5e58
Antti Kivimäki <antti.kivimaki@futurice.com>
2018-08-11 12:46:53 +0300

    Deprecate echo_count and last_echo columns

---
+++ migrations/20180811123722_deprecate_echo_count_and_last_echo.ts
@@ -0,0 +1,18 @@
+import * as Knex from 'knex';
+
+export const up = async (knex: Knex) => {
+  await knex.schema.alterTable('expls', (t) => {
+    t.dropColumn('echo_count');
+    t.dropColumn('last_echo');
+  });
+};
+
+export const down = async (knex: Knex) => {
+  await knex.schema.alterTable('expls', (t) => {
+    t.integer('echo_count')
+      .defaultTo(0)
+      .comment('How many times this expl has been requested');
+    t.timestamp('last_echo')
+      .comment('Last time this expl was echoed somewhere');
+  });
+};
--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import * as _ from 'lodash';
 import { Context } from './types/telegraf';
 import Logger from './logger';
 import * as messages from './constants/messages';
-import { updateExpl } from './database';
+import { addEcho } from './database';
 
 const logger = new Logger(__filename);
 
@@ -11,7 +11,7 @@ export const sendExpl = async (ctx: Context, key: string, expl: Table.Expl | nul
     return ctx.reply(messages.errors.notFound(key));
   }
 
-  await updateExpl(expl, ctx.state);
+  await addEcho(expl, ctx.state);
 
   if (expl.value) {
     return ctx.reply(`${expl.key}: ${expl.value}`);
