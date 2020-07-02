40721ece16489eedfa88502ccd42a06c0ca0925f
Antti Kivimäki <antti.kivimaki@futurice.com>
2020-07-02 16:26:16 +0300

    Remove IS_BORISBOT env. variable since databases are currently separated

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,6 @@ export const env = {
   test: process.env.NODE_ENV === 'test',
 };
 
-export const isBorisBot = Boolean(process.env.IS_BORISBOT);
-
 export const tg = {
   token: process.env.TELEGRAM_TOKEN,
   webhook: process.env.WEBHOOK_DOMAIN,
