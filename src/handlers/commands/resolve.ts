3c667db855659f4cca1d13c63e763446fd6c11f9
Antti Sartjärvi <antti.sartjarvi@gmail.com>
2024-04-11 18:45:34 +0300

    Fix resolve when explCreator not found

--- src/handlers/commands/resolve.ts
+++ src/handlers/commands/resolve.ts
@@ -6,6 +6,7 @@ import { escapeMarkdown, formatYear } from '../../utils';
 import { reactionsKeyboard } from '../events/reaction';
 import * as messages from '../../constants/messages';
 import type { Context } from '../../types/telegraf';
+import logger from '../../logger';
 
 export async function resolveRexpl(ctx: Context) {
   const message = ctx.message as Message.TextMessage;
@@ -26,15 +27,26 @@ export async function resolveRexpl(ctx: Context) {
 
   const key = escapeMarkdown(expl.key);
   const year = formatYear(expl.created_at);
-
-  const chatMember = await ctx.getChatMember(expl.user_id);
-  const explCreator = chatMember?.user.username;
-
-  const reply = explCreator
-    ? `${key}, _${explCreator.replace(/_/, '\\_')} ${year}_`
-    : `${key}, _${year}_`;
-
   const keyboard = await reactionsKeyboard(expl.id);
+
+  let reply = `${key}, _${year}_`;
+
+  // getChatMember throws an error if member not found.
+  // This happens in private messages with the bot, for example.
+  try {
+    const chatMember = await ctx.getChatMember(expl.user_id);
+    const explCreator = chatMember?.user.username;
+
+    if (explCreator) {
+      reply = `${key}, _${explCreator.replace(/_/, '\\_')} ${year}_`;
+    }
+  } catch (error) {
+    const description = _.get(error, ['response', 'description']);
+
+    if (description !== 'Bad Request: user not found') {
+      logger.error(error);
+    }
+  }
 
   await ctx.replyWithMarkdown(reply, {
     reply_markup: keyboard,
